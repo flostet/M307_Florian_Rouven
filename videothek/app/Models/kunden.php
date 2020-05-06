e82301d1720dbb53d18e09a41ff6a5957edd7486
xRouvenx <58586856+xRouvenx@users.noreply.github.com>
2020-05-06 10:55:38 +0200

    Update
    Update

--- videothek/app/Models/kunden.php
+++ videothek/app/Models/kunden.php
@@ -1,58 +1,56 @@
 <?php
 
 class Kunden {
-    public $kundenID;
+    public $kundenid;
     public $vorname;
     public $nachname;
     public $email;
     public $telefon;
-    public FK_Mitgliederstatus_id;
-
+    public $FK_Mitgliederstatus_id;
 
     /**
-     * address constructor.
-     * @param $addressID
-     * @param $name
+     * Kunden constructor.
+     * @param $kundenid
+     * @param $vorname
+     * @param $nachname
      * @param $email
-     * @param $telephone
-     * @param $addressL1
-     * @param $addressL2
+     * @param $telefon
+     * @param $mitgliederstatus
      */
-    public function __construct($name=null, $email=null, $telephone=null, $addressL1=null, $addressL2=null)
+    public function __construct($vorname=null, $nachname=null, $email=null, $telefon=null, $mitgliederstatus=null)
     {
-        $this->name = $name;
+        $this->vorname = $vorname;
+        $this->nachname = $nachname;
         $this->email = $email;
-        $this->telephone = $telephone;
-        $this->addressL1 = $addressL1;
-        $this->addressL2 = $addressL2;
+        $this->telefon = $telefon;
+        $this->FK_Mitgliederstatus_id = $mitgliederstatus;
     }
 
-
     public function create(){
-        $statement = connectToDatabase()->prepare('INSERT INTO `address` (name, email, telephone, addressL1, addressL2) VALUES (:name, :email, :telephone, :addressL1, :addressL2)');
-        $statement->bindParam(':name', $this->name, PDO::PARAM_STR);
+        $statement = connectToDatabase()->prepare('INSERT INTO `kunden` (vorname, nachname, email, telefon, Mitgliederstatus) VALUES (:vorname, :nachname, :email, :telefon, :Mitgliederstatus)');
+        $statement->bindParam(':vorname', $this->vorname, PDO::PARAM_STR);
+        $statement->bindParam(':nachname', $this->nachname, PDO::PARAM_STR);
         $statement->bindParam(':email', $this->email, PDO::PARAM_STR);
-        $statement->bindParam(':telephone', $this->telephone, PDO::PARAM_STR);
-        $statement->bindParam(':addressL1', $this->addressL1, PDO::PARAM_STR);
-        $statement->bindParam(':addressL2', $this->addressL2, PDO::PARAM_STR);
+        $statement->bindParam(':telefon', $this->telefon, PDO::PARAM_STR);
+        $statement->bindParam(':Mitgliederstatus', $this->Mitgliederstatus, PDO::PARAM_STR);
         return $statement->execute();
         $statement = null;
     }
 
     public static function getAll(){
-        $statement = connectToDatabase()->prepare('SELECT * FROM address');
+        $statement = connectToDatabase()->prepare('SELECT * FROM kunden');
         $statement->execute();
         $result = $statement->fetchAll();
-        $addresses = [];
+        $kunden = [];
         foreach($result as $a) {
-            $addresses[] = $a;
+            $kunden[] = $a;
         }
-        return $addresses;
+        return $kunden;
         $statement = null;
     }
 
     public static function countAll(){
-        $statement = connectToDatabase()->prepare('SELECT COUNT(`addressID`) FROM `address`');
+        $statement = connectToDatabase()->prepare('SELECT COUNT(`kundenid`) FROM `kunden`');
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_COLUMN);
         return $result[0];
@@ -60,24 +58,25 @@ class Kunden {
     }
 
     private static function dbResultToTask($a){
-        return new Address($a['addressID'], $a['name'], $a['$email'], $a['telephone'], $a['addressL1'], $a['addressL2']);
+        return new Kunden($a['kundenid'], $a['vorname'], $a['nachname'], $a['$email'], $a['telefon'], $a['mitgliederstatus']);
     }
 
     public function update()
     {
-        $statement = connectToDatabase()->prepare('UPDATE `address` 
+        $statement = connectToDatabase()->prepare('UPDATE `kunden` 
         SET 
-        name = :name,
+        vorname = :vorname,
+        nachname = :nachname,
         email = :email,
-        telephone = :telephone,
-        addressL1 = :addressL1,
-        addressL2 = :addressL2
-        WHERE addressId = :addressId');
-        $statement->bindParam(':name', $this->name);
+        telefon = :telefon,
+        mitgliederstatus = :mitlgiederstatus
+        WHERE kundenid = :kundenid');
+        $statement->bindParam(':vorname', $this->vorname);
+        $statement->bindParam(':nachname', $this->nachname);
         $statement->bindParam(':email', $this->email);
-        $statement->bindParam(':addressL1', $this->addressL1);
-        $statement->bindParam(':addressL2', $this->addressL2);
-        $statement->bindParam(':addressId', $this->addressId);
+        $statement->bindParam(':telefon', $this->telefon);
+        $statement->bindParam(':mitgliederstatus', $this->mitgliederstatus);
+        $statement->bindParam(':kundenid', $this->kundenid);
         return $statement->execute();
         $statement = null;
     }
--- videothek/app/Views/ausleiheerfassen.view.php
+++ videothek/app/Views/ausleiheerfassen.view.php
@@ -1,1 +1,82 @@
-<?php
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Meine Seite</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href='public/css/app.css' rel="stylesheet" >
+</head>
+<body>
+
+<div class="topnav"> 
+<a class="active" href="home">Home</a> 
+    <a href="ausleiheerfassen">Ausleihe Erfassen</a> 
+    <a href="ausleihen">Ausleihe Liste</a> 
+    </div>
+
+<h1>Ausleihe erfassen</h1>
+
+<p>Bitte Ausleihe erfassen</p>
+
+<?php if(count($errors) > 0): ?>
+    <ul class="alert alert-block">
+        <?php foreach($errors as $error): ?>
+            <li><?= $error ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endif; ?>
+
+<?php if($newEntry): ?>
+    <div class="alert alert-success">
+        <strong>Danke!</strong> Für Eintrag. OK, hat klappt!
+    </div>
+<?php endif; ?>
+
+<form action="index.php?page=ausleiheerfassenController.php" method="post">
+    <div class="control-group">
+        <label class="control-label" for="vorname">Vorname:</label>
+        <input value="<?= $vorname ?>" name="vorname" type="text" class="input-xlarge" id="vorname">
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="nachname">Nachname:</label>
+        <input value="<?= $nachname ?>" name="nachname" type="text" class="input-xlarge" id="nachname">
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="email">Email:</label>
+        <textarea name="email" class="input-xlarge" id="email"><?= $email ?></textarea>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="ausgelehntes video">Ausgelehntes Video:</label>
+        <input value="<?= $name ?>" name="ausgelehntes video" type="text" class="input-xlarge" id="ausgelehntes video">
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="telefon">Telefon:</label>
+        <input value="<?= $name ?>" name="telefon" type="text" class="input-xlarge" id="telefon">
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="mitgliederstatus">Mitgliederstatus:</label>
+        <input value="<?= $name ?>" name="mitgliederstatus" type="text" class="input-xlarge" id="mitgliederstatus">
+    </div>
+    
+    <div class="control-group">
+        <button type="submit" class="btn btn-primary">Ausleihen</button>
+    </div>
+    <div class="control-group">
+        <button type="cancel" class="btn btn-primary">Abbrechen</button>
+    </div>
+</form>
+<hr>
+<div class="ausleiheerfassen">
+    <?php foreach($ausleiheerfassenEntries as $entry): ?>
+    <div class="ausleiheerfassen__entry">
+        <h4><?= $entry['vorname'] ?></h4>
+        <h4><?= $entry['nachname'] ?></h4>
+        <h4><?= $entry['email'] ?></h4>
+        <h4><?= $entry['ausgelehntes video'] ?></h4>
+        <h4><?= $entry['telefon'] ?></h4>
+        <h4><?= $entry['mitgliederstatus'] ?></h4>
+        <hr>
+    </div>
+    <?php endforeach; ?>
+</div>
+
